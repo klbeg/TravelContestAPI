47dbbd221a9119dc9769f4d535371105af41ae21
B33Gs <speschk@yahoo.com>
2025-06-27 15:49:29 -0400

    refactor index.js to handle controller injection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { makeServer } = require('./server')
 const { controllers } = require('./controllers/index')
 
-const server = makeServer(controllers)
-
 const PORT = process.env.SERVER_PORT || 5000
+
+const server = makeServer(controllers)
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`))
