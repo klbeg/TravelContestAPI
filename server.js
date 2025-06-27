1fd0abce1decbbf484bd6c6ea285ef654ba8f41a
B33Gs <speschk@yahoo.com>
2025-06-27 15:57:31 -0400

    add error handling to server for bad routes

--- server.js
+++ server.js
@@ -8,9 +8,25 @@ function makeServer(controllers) {
         switch (url) {
           case '/api/players':
             controllers.getPlayers(req, res)
+            break
+          default:
+            res.writeHead(400, { 'Content-Type': 'application/json' })
+            res.end(
+              JSON.stringify({
+                message: "The specified route doesn't exist",
+              })
+            )
+            break
         }
+        break
+      default:
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({ message: "The specified route doesn't exist" })
+        )
     }
   })
+
   return (this.server = server)
 }
 
