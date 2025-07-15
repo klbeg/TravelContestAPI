71988507a2fb3851b5a1eaaca53ce50dd9f2b594
B33Gs <speschk@yahoo.com>
2025-07-15 18:48:11 -0400

    push up most up to date docker progress

--- api/server.js
+++ api/server.js
@@ -3,17 +3,18 @@ const http = require('http')
 function makeServer(controllers) {
   const server = http.createServer((req, res) => {
     const { method, url } = req
+    console.log('url', url.split('/'))
     switch (method) {
       case 'GET':
         switch (url) {
           case '/api/players':
-            controllers.getPlayers(req, res)
-            // res.writeHead(200, { 'Content-Type': 'application/json' })
-            // res.end(
-            //   JSON.stringify({
-            //     message: 'docker container running as expected',
-            //   })
-            // )
+            // controllers.getPlayers(req, res)
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(
+              JSON.stringify({
+                message: 'docker container running as expected',
+              })
+            )
             break
           default:
             res.writeHead(400, { 'Content-Type': 'application/json' })
